9562bc71aa9112dae6c8afcd929dfea54f772fff
LeonardoHui <leonardo.hui.learning@gmail.com>
2023-01-07 14:15:29 -0300

    feat: add page 404

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Copyright &copy; MyFinances</p>
+      <p>Copyright &copy;MyFinances</p>
       <p>
         <Link href="/about">About this project</Link>
       </p>
