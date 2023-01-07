2efade04a201572b23aacc949e3b87b475e37630
LeonardoHui <leonardo.hui.learning@gmail.com>
2023-01-07 16:14:12 -0300

    feat: add showcase img to homepage

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import Header from "./Header";
 import Footer from "./Footer";
-import styles from "../styles/Layout.module.css";
+import Showcase from "./Showcase";
+import styles from "@/styles/Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -13,7 +17,11 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
+
       <Header />
+
+      {router.pathname === "/" && <Showcase />}
+
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
