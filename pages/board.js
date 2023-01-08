674f6d274419d94def5fcd09d5b36bedf1be03a4
LeonardoHui <leonardo.hui.learning@gmail.com>
2023-01-07 21:07:14 -0300

    feat: add api route

---
+++ pages/board.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+
+import { API_URL } from "@/config/index";
+
+export default function BoardPage({ events }) {
+  return (
+    <Layout>
+      <h1>Upcoming events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/request}`);
+  const events = await res.json;
+
+  return {
+    props: { events },
+  };
+}
